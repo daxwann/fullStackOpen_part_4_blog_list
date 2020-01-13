1a9d0a30b8c5ab2492ed4854702d0aefa0202bac
Dax Wann <daxwann@gmail.com>
2020-01-13 12:43:11 -0800

    install packages and set up env

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+// const http = require('http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// load env in development
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number
-})
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
+const mongoUrl = process.env.MONGO_URL || 'mongodb://db:27017/blog-list';
 
-app.use(cors())
-app.use(bodyParser.json())
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
-      response.json(blogs)
-    })
-})
+      response.json(blogs);
+    });
+});
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+  const blog = new Blog(request.body);
 
   blog
     .save()
     .then(result => {
-      response.status(201).json(result)
-    })
-})
+      response.status(201).json(result);
+    });
+});
 
-const PORT = 3003
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
