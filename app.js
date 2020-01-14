6d8447432326257c3bb612b0b368c5694c09dfb2
Dax Wann <daxwann@gmail.com>
2020-01-14 09:03:57 -0800

    add app.js

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// load env in development
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const mongoUrl = process.env.MONGODB_URI || 'mongodb://db:27017/blog-list';
+
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs);
+    });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result);
+    });
+});
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,53 +1,8 @@
-// const http = require('http');
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const app = require('./app');
+const http = require('http');
 
-// load env in development
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+const server = http.createServer(app);
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = process.env.MONGODB_URI || 'mongodb://db:27017/blog-list';
-
-mongoose.connect(mongoUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-app.use(cors());
-app.use(bodyParser.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
