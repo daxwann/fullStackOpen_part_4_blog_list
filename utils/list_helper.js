99f48425a6ae3bb2a5c637cbe544284ef127c020
Dax Wann <daxwann@gmail.com>
2020-01-14 10:48:14 -0800

    complete ex4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, currBlog) => {
+    return sum + currBlog.likes;
+  };
+
+  return blogs.reduce(reducer, 0);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
